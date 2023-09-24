2a267dd1195ef2cac7fdce68be402948c21f9d62
Ravi Prakash <raviprakash.181it137@nitk.edu.in>
2023-09-24 14:58:39 +0530

    error component update

---
+++ e02-Igniting-our-app/src/components/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div className="about-container">
+      <h1>
+        Hello I am Ravi Prakash currently learning Reactjs and looking
+        opportunity to demonstrate my skills.
+      </h1>
+    </div>
+  );
+};
+export default About;
